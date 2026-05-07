cde0739fac3a0d45b602425bc4f2454b7d250844
Lucas <lucasmanarimdias@gmail.com>
2026-05-07 14:38:35 -0300

    primeiro commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,32 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import {StyleSheet, View, Text, Button} from 'react-native';
+import {useState} from 'react';
 
 export default function HomeScreen() {
+  const [num, setNum] = useState(0);
+   const [text, setText] = useState("");
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Hello World!</Text>
+        <Button title="+" onPress={() => setNum(num + 1)} />
+        
+        <Text>{num}</Text>
+
+        <Button title="-" onPress={() => setNum(num - 1)} />
+
+        <Button title="Reset" onPress={() => setNum(0)} />
+        
+        {num < 0 && <Text style={{ color: 'red' }}>Negative</Text>}
+        {num > 0 && <Text style={{ color: 'green' }}>Positive</Text>}
+        {num === 0 && <Text style={{ color: 'blue' }}>Zero</Text>}
+        {text.length > 0 && <Text>{text}</Text>}
+        {num === 10 && <Text style={{ color: 'purple' }}>10</Text>}
+
+
+        <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}  
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+      </View>
+    );
+  }
---
+++ app/(tabs)/outratela.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function SegundaTela() {
+  return (
+    <View>
+      <Text>Segunda Tela</Text>
+    </View>
+  );
+}
