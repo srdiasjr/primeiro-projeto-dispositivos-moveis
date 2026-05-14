ad13cbb4b62beb2fd8aead3785bd0e10c87f337d
Lucas <lucasmanarimdias@gmail.com>
2026-05-14 16:29:36 -0300

    mudanças gerais e finalizaçao

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,47 +1,112 @@
-import react from 'react';
-import {View,Text,StyleSheet,Image,ScrollView,TouchableOpacity,TextInput} from 'react-native';
-import { useState } from 'react';
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function Index() {
-  
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-        },
-        perfil: {
-            width: 200,
-            height: 200,
-            borderRadius: 100,
-            borderWidth: 1,
 
-            position: 'absolute',
-            top: 110,
-        },
-        inputnome: {
-            width: 200,
-            height: 40,
-            borderRadius: 10,
-            position: 'absolute',
-            top: 350,
-            backgroundColor: '#686B8D',
-            paddingLeft: 15,
-            placeholderTextColor: '#ffffff86',
-        },
-        
-        });
+  const styles = StyleSheet.create({
 
-    return (
-    <View>
-        <View style={styles.container}>
-                <Image
-                    style={styles.perfil}
-                    source={require('../../assets/images/logo.png')} />
-            </View>
-            <View style={styles.container}>
-                <TextInput placeholder='Digite seu nome de usuario' style={styles.inputnome}/>
-            </View>
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    perfil: {
+      width: 200,
+      height: 200,
+      borderRadius: 100,
+      borderWidth: 1,
+
+      position: 'absolute',
+      top: 110,
+    },
+
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+
+      width: 210,
+      height: 40,
+
+      backgroundColor: '#686B8D',
+      borderRadius: 10,
+
+      position: 'absolute',
+      top: 350,
+
+      paddingHorizontal: 4,
+    },
+
+    icone: {
+      fontSize: 20,
+      marginRight: 8,
+      color: 'white',
+    },
+
+    inputnome: {
+      flex: 1,
+      color: 'white',
+    },
+    inputsenha: {
+      flex: 1,
+      color: 'white',
+    },
+    botaoentrar: {
+        width: 150,
+        height: 40,
+        backgroundColor: '#686B8D',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+
+  });
+
+  return (
+
+    <View style={styles.container}>
+    {/*Foto de perfil*/}
+      <Image
+        style={styles.perfil}
+        source={require('../../assets/images/logo.png')}
+      />
+
+    {/*Input nome de usuario*/}
+      <View style={styles.inputContainer}>
+
+        <Text style={styles.icone}>👤</Text>
+
+        <TextInput
+          placeholder='Digite seu nome de usuario'
+          placeholderTextColor='#ffffff86'
+          style={styles.inputnome}
+        />
+
+      </View>
+
+        <View style={[styles.inputContainer, { top: 410 }]}>
+            <Text style={styles.icone}>🔒</Text>
+
+            <TextInput
+            placeholder='Digite sua senha'
+            secureTextEntry={true}
+            placeholderTextColor='#ffffff86'
+            style={styles.inputsenha}
+            />
+        </View>
+
+        <View style={{ position: 'absolute', top: 470 }}>
+            <TouchableOpacity style={styles.botaoentrar}>
+                <Text style={{ color: 'white' }}>Entrar</Text>
+            </TouchableOpacity>
+        </View>
     </View>
   );
-    }
+}
